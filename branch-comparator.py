e4697eb6de6a5dc1e3328cc6faedaa99ec3467c0
Alexei Kartashov <alexei.zebra@yandex.ru>
2024-11-17 15:24:16 +0300

    init commit

---
+++ branch-comparator.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+# Utility for comparing altlinux branches
+
+# Copyright (C) 2024  Alexei Kartashov <zebra@alt-gnome.ru>
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
---
+++ readme.md
@@ -0,0 +1,1 @@
+# Utility for comparing altlinux branches.
